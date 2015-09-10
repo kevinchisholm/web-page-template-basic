d138e1ed4a1915a1d82fbc8534694baca75239f2
kevin <kevin@News-MacBook-Pro.local>
2015-09-10 17:45:39 -0400

    add core files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function(_win, $){
+
+    'use strict';
+
+    /*global window: false */
+
+    $(document).ready(function(){
+        $('body').append('<div class="box"><p style="color:#008080;font-weight:bold;">This box was added by index.js</p></div>');
+    });
+
+})(window, window.jQuery);
